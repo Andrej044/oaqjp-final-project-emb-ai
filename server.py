663596f51e299b99e535d8b77edc8416e07e8e47
Andrii Lishchyna <andrej044@i.ua>
2024-03-06 23:10:38 -0500

    add render main page template

--- server.py
+++ server.py
@@ -8,3 +8,8 @@ def emo_detector():
   text_to_analyze = request.args.get('textToAnalyze')
   response = emo_detector(text_to_analyze)
   return response
+
+@app.route('/')
+def render_index_page():
+  return render_template('index.html')
+
