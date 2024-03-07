0933039578e5190984be2b2540b4d60e7bec1ce7
Andrii Lishchyna <andrej044@i.ua>
2024-03-07 11:44:41 -0500

    render error if response status code = 400

--- server.py
+++ server.py
@@ -7,7 +7,12 @@ app = Flask('Emotion Detector')
 def emo_detector():
   text_to_analyze = request.args.get('textToAnalyze')
   response = emotion_detector(text_to_analyze)
-  return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['domination_emotion']}."
+  domination_emotion = response['domination_emotion']
+  
+  if domination_emotion is None:
+    return 'Invalid text! Please try again!.'
+  else:
+    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['domination_emotion']}."
 
 @app.route('/')
 def render_index_page():
