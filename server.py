9e479cc127adee7372341e3462a1e6c53a0c9dac
Andrii Lishchyna <andrej044@i.ua>
2024-03-06 23:31:34 -0500

    fixed error, add returned statement

--- server.py
+++ server.py
@@ -6,10 +6,12 @@ app = Flask('Emotion Detector')
 @app.route('/emotionDetector')
 def emo_detector():
   text_to_analyze = request.args.get('textToAnalyze')
-  response = emo_detector(text_to_analyze)
-  return response
+  response = emotion_detector(text_to_analyze)
+  return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['domination_emotion']}."
 
 @app.route('/')
 def render_index_page():
   return render_template('index.html')
 
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000)
