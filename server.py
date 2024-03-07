e1a60bd0ae62d8b85b5e38541c7d0183646fd035
Andrii Lishchyna <andrej044@i.ua>
2024-03-06 23:01:15 -0500

    add route "/emotionDetector"

--- server.py
+++ server.py
@@ -1,2 +1,10 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask('Emotion Detector')
+
+@app.route('/emotionDetector')
+def emo_detector():
+  text_to_analyze = request.args.get('textToAnalyze')
+  response = emo_detector(text_to_analyze)
+  return response
